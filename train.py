7f1cf6d357842487cc64bf86bdb4df3970627cd5
Samar Khanna <samar.khanna@gmail.com>
2021-12-04 12:23:45 -0800

    Comments

--- train.py
+++ train.py
@@ -7,9 +7,6 @@ from torch_geometric.utils import negative_sampling
 
 
 def train(model, link_predictor, emb, edge_index, split_edge, batch_size, optimizer, device):
-    '''
-    Train the model
-    '''
 
     model.train()
     link_predictor.train()
@@ -25,14 +22,14 @@ def train(model, link_predictor, emb, edge_index, split_edge, batch_size, optimi
     for edge_id in DataLoader(range(pos_train_edge.shape[0]), batch_size, shuffle=True):
         optimizer.zero_grad()
 
-        node_emb = model(emb, edge_index)
-        pos_edge = pos_train_edge[edge_id].T
+        node_emb = model(emb, edge_index)  # (N, d)
 
-        pos_pred = link_predictor(node_emb[pos_edge[0]], node_emb[pos_edge[1]])
+        pos_edge = pos_train_edge[edge_id].T  # (2, B)
+        pos_pred = link_predictor(node_emb[pos_edge[0]], node_emb[pos_edge[1]])  # (B, )
+
         neg_edge = negative_sampling(edge_index, num_nodes=emb.shape[0],
-                                     num_neg_samples=edge_id.shape[0], method='dense')
-
-        neg_pred = link_predictor(node_emb[neg_edge[0]], node_emb[neg_edge[1]])
+                                     num_neg_samples=edge_id.shape[0], method='dense')  # (Ne,2)
+        neg_pred = link_predictor(node_emb[neg_edge[0]], node_emb[neg_edge[1]])  # (Ne,)
 
         loss = -torch.log(pos_pred + 1e-15).mean() - torch.log(1 - neg_pred + 1e-15).mean()
         loss.backward()
