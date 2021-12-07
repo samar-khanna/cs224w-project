d74fd853db8960776316eff834ba46491c0dcac1
SConsul <sarthakconsul@gmail.com>
2021-12-06 20:14:08 -0800

    fixed eval logger

--- online_eval.py
+++ online_eval.py
@@ -4,7 +4,7 @@ import numpy as np
 import copy
 from tqdm import trange
 from torch_geometric.data import DataLoader
-from utils import print_and_log
+from utils import log
 
 def online_eval(model, link_predictor, emb, edge_index, pos_edges, neg_edges, batch_size,res_file):
     model.eval()
@@ -18,7 +18,7 @@ def online_eval(model, link_predictor, emb, edge_index, pos_edges, neg_edges, ba
         node_emb = model(emb, edge_index)  # (N, d)
 
         pos_edge = pos_edges[edge_id].T  # (2, B)
-        pos_pred = link_predictor(node_emb[pos_edge[0]], node_emb[pos_edge[1]])  # (B, )
+        pos_pred = link_predictor(node_emb[pos_edge[0]], node_emb[pos_edge[1]]).squeeze()  # (B, )
 
         tp += (pos_pred >= 0.5).sum()
         fn += (pos_pred < 0.5).sum()
@@ -27,11 +27,11 @@ def online_eval(model, link_predictor, emb, edge_index, pos_edges, neg_edges, ba
         node_emb = model(emb, edge_index)  # (N, d)
 
         neg_edge = neg_edges[edge_id].T  # (2, B)
-        neg_pred = link_predictor(node_emb[neg_edge[0]], node_emb[neg_edge[1]])  # (B, )
+        neg_pred = link_predictor(node_emb[neg_edge[0]], node_emb[neg_edge[1]]).squeeze()  # (B, )
 
         fp += (neg_pred >= 0.5).sum()
         tn += (neg_pred < 0.5).sum()
 
     if res_file is not None:
-        print_and_log(res_file,f"corr_pred = {pos_edge.T[pos_pred >= 0.5]} \ninc_pred = {neg_edge.T[neg_pred >= 0.5]}")
+        log(res_file,f"corr_pred = {pos_edge.T[pos_pred >= 0.5]} \ninc_pred = {neg_edge.T[neg_pred >= 0.5]}")
     return tp, tn, fp, fn
