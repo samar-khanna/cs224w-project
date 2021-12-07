ade063d4884d68c40f2b20c33f23406727106d84
SConsul <sarthakconsul@gmail.com>
2021-12-07 12:52:19 -0800

    added Hits evaluation

---
+++ evaluate.py
@@ -0,0 +1,53 @@
+import torch
+from torch_geometric.data import DataLoader
+
+def test(model, predictor, emb, edge_index, split_edge, batch_size, evaluator):
+    model.eval()
+    predictor.eval()
+
+    node_emb = model(emb, edge_index)
+
+    pos_valid_edge = split_edge['valid']['edge'].to(emb.device)
+    neg_valid_edge = split_edge['valid']['edge_neg'].to(emb.device)
+    pos_test_edge = split_edge['test']['edge'].to(emb.device)
+    neg_test_edge = split_edge['test']['edge_neg'].to(emb.device)
+
+    pos_valid_preds = []
+    for perm in DataLoader(range(pos_valid_edge.size(0)), batch_size):
+        edge = pos_valid_edge[perm].t()
+        pos_valid_preds += [predictor(node_emb[edge[0]], node_emb[edge[1]]).squeeze().cpu()]
+    pos_valid_pred = torch.cat(pos_valid_preds, dim=0)
+
+    neg_valid_preds = []
+    for perm in DataLoader(range(neg_valid_edge.size(0)), batch_size):
+        edge = neg_valid_edge[perm].t()
+        neg_valid_preds += [predictor(node_emb[edge[0]], node_emb[edge[1]]).squeeze().cpu()]
+    neg_valid_pred = torch.cat(neg_valid_preds, dim=0)
+
+    pos_test_preds = []
+    for perm in DataLoader(range(pos_test_edge.size(0)), batch_size):
+        edge = pos_test_edge[perm].t()
+        pos_test_preds += [predictor(node_emb[edge[0]], node_emb[edge[1]]).squeeze().cpu()]
+    pos_test_pred = torch.cat(pos_test_preds, dim=0)
+
+    neg_test_preds = []
+    for perm in DataLoader(range(neg_test_edge.size(0)), batch_size):
+        edge = neg_test_edge[perm].t()
+        neg_test_preds += [predictor(node_emb[edge[0]], node_emb[edge[1]]).squeeze().cpu()]
+    neg_test_pred = torch.cat(neg_test_preds, dim=0)
+
+    results = {}
+    for K in [20, 50, 100]:
+        evaluator.K = K
+        valid_hits = evaluator.eval({
+            'y_pred_pos': pos_valid_pred,
+            'y_pred_neg': neg_valid_pred,
+        })[f'hits@{K}']
+        test_hits = evaluator.eval({
+            'y_pred_pos': pos_test_pred,
+            'y_pred_neg': neg_test_pred,
+        })[f'hits@{K}']
+
+        results[f'Hits@{K}'] = (valid_hits, test_hits)
+
+    return results
