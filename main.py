9916911dff1f725ed14260fdc150425c2b9a9b6f
SConsul <sarthakconsul@gmail.com>
2021-12-07 12:52:40 -0800

    added eval, argparse

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import torch
-from ogb.linkproppred import PygLinkPropPredDataset
+from ogb.linkproppred import PygLinkPropPredDataset, Evaluator
 from torch.optim import optimizer
 from torch_geometric.data import DataLoader
 from gnn_stack import GNNStack
 from train import train
 from link_predictor import LinkPredictor
 import torch.optim as optim
-
+from evaluate import test
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description="Script to train link prediction in offline graph setting")
+    parser.add_argument('--epochs', type=int, default=50,
+                        help="Number of epochs for training")
+    parser.add_argument('--lr', type=float, default=3e-3,
+                        help="Learning rate training")
+    parser.add_argument('--node_dim', type=int, default=256,
+                        help='Embedding dimension for nodes')
+    parser.add_argument('--dropout', type=float, default=0.3) #0.5
+    parser.add_argument('--batch_size', type=int, default=64 * 1024)
+    parser.add_argument('--num_layers', type=int, default=2) #4
+    parser.add_argument('--hidden_channels', type=int, default=256) #32
+
+    args = parser.parse_args()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    epochs = 50
-    hidden_dim = 32
-    dropout = 0.5
-    num_layers = 4
-    lr = 1e-2
     optim_wd = 0
-    node_emb_dim = 256
-    batch_size = 64*1024
+    epochs = args.epochs
+    hidden_dim = args.hidden_channels
+    dropout = args.dropout
+    num_layers = args.num_layers
+    lr = args.lr
+    node_emb_dim = args.node_dim
+    batch_size = args.batch_size
 
     # Download and process data at './dataset/ogbl-ddi/'
     dataset = PygLinkPropPredDataset(name="ogbl-ddi", root='./dataset/')
@@ -28,6 +42,7 @@ def main():
     graph = dataset[0]
     edge_index = graph.edge_index.to(device)
 
+    evaluator = Evaluator(name='ogbl-ddi')
     # train_loader = DataLoader(dataset[split_edge["train"]], batch_size=32, shuffle=True)
     # val_loader = DataLoader(dataset[split_edge["valid"]], batch_size=32, shuffle=False)
     # test_loader = DataLoader(dataset[split_edge["test"]], batch_size=32, shuffle=False)
@@ -42,8 +57,10 @@ def main():
     )
     for e in range(epochs):
         loss = train(model, link_predictor, emb.weight, edge_index, pos_train_edge, batch_size, optimizer)
-        print(f"Epoch {e + 1} loss: {round(loss, 5)}")
+        print(f"Epoch {e + 1}: loss: {round(loss, 5)}")
 
-
+        if (e+1)%5 ==0:
+            result = test(model, link_predictor, emb.weight, edge_index, split_edge, batch_size, evaluator)
+            print(result)
 if __name__ == "__main__":
     main()
